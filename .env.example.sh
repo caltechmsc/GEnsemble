92d246a38c402e86045b2011d17c2e5f8dc3d3e6
Tony Kan <tianchengkan@gmail.com>
2025-02-15 13:47:05 -0800

    feat(env): Add example environment configuration files for GE tools (csh and bash)

---
+++ .env.example.sh
@@ -0,0 +1,12 @@
+#!/bin/csh
+
+setenv GE_PATH "/groups/wag/GEnsemble"
+setenv PATH "${GE_PATH}/programs:${PATH}"
+
+alias ge_blast "${GE_PATH}/programs/Blast.pl"
+alias ge_predictm "${GE_PATH}/programs/PredicTM.pl"
+alias ge_opthelix "${GE_PATH}/programs/OptHelix.pl"
+alias ge_template "${GE_PATH}/programs/Align2Template.pl"
+alias ge_bihelix "${GE_PATH}/programs/bihelixrot2/all_bihelix_pairs.pl"
+alias ge_bihelixanal "${GE_PATH}/programs/bihelixrot2/bihelix_anal.pl"
+alias ge_combihelix "${GE_PATH}/programs/bihelixrot2/combi_frombihelixsum.pl"
